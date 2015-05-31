e58002d6dae245a9cbb53887eabe173abd717027
plasante <pierre.lasante@videotron.ca>
2015-05-31 06:50:45 -0700

    reached ContentNegotiatingViewResolver

--- src/main/java/com/uniksoft/webstore/controller/ProductController.java
+++ src/main/java/com/uniksoft/webstore/controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.uniksoft.webstore.controller;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.uniksoft.webstore.entity.Product;
 import com.uniksoft.webstore.service.ProductService;
@@ -70,17 +74,27 @@ public class ProductController {
 	@InitBinder
 	public void initialiseBinder(WebDataBinder binder) {
 		binder.setDisallowedFields("unitsInOrder","discontinued");
+		binder.setAllowedFields("productId","name","condition","unitPrice","description","manufacturer","category","unitsInStock","productImage");
 	}
 	
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
 	public String processAddNewProductForm(@ModelAttribute("newProduct") Product newProduct,
-										BindingResult result) {
+										BindingResult result, HttpServletRequest request) {
 		String[] suppressedFields = result.getSuppressedFields();
 		if(suppressedFields.length > 0) {
 			throw new RuntimeException("Attempting to bind disallowed fields: " + 
 						StringUtils.arrayToCommaDelimitedString(suppressedFields));
 		}
-		
+		MultipartFile productImage = newProduct.getProductImage();
+		String rootDirectory = request.getSession().getServletContext().getRealPath("/");
+		if(productImage != null && !productImage.isEmpty()) {
+			try {
+				String path = rootDirectory + "resources/images/" + newProduct.getProductImage() + ".png";
+				productImage.transferTo(new File(path));
+			} catch(Exception e) {
+				throw new RuntimeException("Product Image saving failed", e);
+			}
+		}
 		productService.addProduct(newProduct);
 		return "redirect:/products";
 	}
--- src/main/webapp/WEB-INF/views/addProduct.jsp
+++ src/main/webapp/WEB-INF/views/addProduct.jsp
@@ -16,7 +16,7 @@
 		</div>
 	</section>
 	<section class="container-fluid">
-		<form:form modelAttribute="newProduct" class="form-horizontal">
+		<form:form modelAttribute="newProduct" class="form-horizontal" enctype="multipart/form-data">
 			<fieldset>
 				<legend>Add new product</legend>
 				<div class="form-group">
@@ -69,6 +69,14 @@
 						<form:radiobutton path="condition" value="New" />New
 						<form:radiobutton path="condition" value="Old" />Old
 						<form:radiobutton path="condition" value="Refurbished" />Refurbished
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="control-label col-lg-2" for="productImage">
+						<spring:message code="addProduct.form.productImage.label" />
+					</label>
+					<div class="col-lg-10">
+						<form:input id="productImage" path="productImage" type="file" class="form:input-large" />
 					</div>
 				</div>
 				<div class="form-group">
